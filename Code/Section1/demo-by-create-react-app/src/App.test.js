2c4f6ef4b5ddb89bf8cb63747983ac405db26366
Gaoxin Huang <gx173139880@qq.com>
2019-07-06 11:28:18 +1200

    finish 10

--- Code/Section1/demo-by-create-react-app/src/App.test.js
+++ Code/Section1/demo-by-create-react-app/src/App.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import Enzyme from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-  // throw new Error;
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+test('renders without crashing', () => {
+
 });
