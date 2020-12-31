a99c4705cf8635a58560f7402983a0941b8d2f3e
Gaoxin_Huang <gx173139880@qq.com>
2020-12-31 21:31:23 +1300

    Update 112

--- Code/section9_base-for-hooks-context/src/Input.js
+++ Code/section9_base-for-hooks-context/src/Input.js
@@ -4,14 +4,44 @@ import PropTypes from 'prop-types';
 
 
 function Input({ secretWord }) {
+  const [currentGuess, setCurrentGuess] = React.useState("");
   return (
     <div data-test='component-input'>
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          // placeholder={stringsModule.getStringByLanguage(language, 'guessInputPlaceholder')}
+          value={currentGuess}
+          onChange={(event) => setCurrentGuess(event.target.value)}
+        />
+        <button
+          data-test="submit-button"
+          onClick={(evt) => {
+            evt.preventDefault();
+            // update guessedWords
+            // const letterMatchCount = getLetterMatchCount(currentGuess, secretWord);
+            // const newGuessedWords = [...guessedWords, { guessedWord: currentGuess, letterMatchCount }];
+            // setGuessedWords(newGuessedWords);
 
+            // check against secretWord and update success if needed
+            // if (currentGuess === secretWord) {
+            //   setSuccess(true);
+            // }
+            // clear input box
+            setCurrentGuess("");
+          }}
+          className="btn btn-primary mb-2">
+          {/* {stringsModule.getStringByLanguage(language, 'submit')} */}
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
 
 Input.propTypes = {
-  secretWord:PropTypes.string.isRequired,
+  secretWord: PropTypes.string.isRequired,
 }
 export default Input;
