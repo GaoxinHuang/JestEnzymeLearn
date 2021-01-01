32d7e12babea4114cb1d1ad55e6bdc6b74357914
Gaoxin_Huang <gx173139880@qq.com>
2021-01-01 15:37:01 +1300

    completed 117

--- Code/section9_base-for-hooks-context/src/App.test.js
+++ Code/section9_base-for-hooks-context/src/App.test.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import App from './App';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { findByTestAttr } from '../test/testUtils';
 
+import hookActions from './actions/hookActions';
+
+const mockGetSecretWord = jest.fn();
+
 const setup = () => {
-  return shallow(<App />);
+  mockGetSecretWord.mockClear();
+  hookActions.getSecretWord = mockGetSecretWord; //mock 这个 getSecretWord
+  //use mount, because useEffect not called on 'shallow'
+  return mount(<App />);
 }
 
 test('future test goes here', () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, 'component-app');
   expect(component.length).toBe(1);
+});
+
+describe('getSecretWord calls', () => {
+  test('getSecretWord gets called on App mount', () => {
+    setup();
+
+    // check to see if secret word was updated
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
+  test('secretWord does not update on App update', () => {
+    const wrapper = setup();
+    mockGetSecretWord.mockClear();
+
+    // wrapper.update() doesn't trigger update
+    // (issue forked from https://github.com/airbnb/enzyme/issues/2091)
+    wrapper.setProps();
+
+    expect(mockGetSecretWord).not.toHaveBeenCalled();
+  });
 });
